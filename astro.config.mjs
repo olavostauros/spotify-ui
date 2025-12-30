bc002a8a81c7782e29e145ee5be8cd39b6b390c9
olavostauros <olavostauros@outlook.com>
2025-12-30 20:16:42 -0300

    Fix GitHub Pages base path for CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    site: "https://olavostauros.github.io",
+    base: "/spotify-ui",
+    integrations: [tailwind()],
 });
